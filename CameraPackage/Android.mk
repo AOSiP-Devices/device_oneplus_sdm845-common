2a4a69fe1ac98b384cd8a8383d668f05b38c726e
Anirudh Gupta <anirudhgupta109@gmail.com>
2018-11-05 09:36:08 +0000

    CameraPackage: Enable LOCAL_PRIVATE_PLATFORM_APIS
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

--- CameraPackage/Android.mk
+++ CameraPackage/Android.mk
@@ -4,6 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := CameraPackage
+LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
