27eb1c4895613b86c163594336961a4065aa05c9
Anirudh Gupta <anirudhgupta109@gmail.com>
2019-06-13 02:33:16 -0700

    sdm845-common: Add OP Camera compatibility
    Change-Id: I6350f7126d972e916202e79f0cf86d003e228195
    Co-authored-by: darkobas <darkobas@gmail.com>
    Co-authored-by: maxwen <max.weninger@gmail.com>
    Co-authored-by: Vachounet <vachounet@live.fr>
    Co-authored-by: luca020400 <luca.stefani.ge1@gmail.com>
    Co-authored-by: Rabih Salamey <somcom3@gmail.com>
    Co-authored-by: hardwareadictos <hardwareadictos@gmail.com>
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -24,7 +24,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
-    camera.disable_zsl_mode=true
+    camera.disable_zsl_mode=true \
+    vendor.camera.aux.packagelist="com.oneplus.camera,org.codeaurora.snapcam"
 
 # Debug IMS/VoLTE props
 PRODUCT_PROPERTY_OVERRIDES += \
