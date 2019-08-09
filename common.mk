8c76a3eb94062f3fac9ce7a8be1cda10e1363c3d
Artem Borisov <dedsa2002@gmail.com>
2019-08-09 23:50:51 +0530

    sdm845-common: Restore QCOM's WFD implementation
    https://review.aosiprom.com/q/topic:guac-aa should address Android Auto not working with the WFD blobs
    
    This reverts commit 016a652933a596dfa65939038443b5c28ee0d603.
    Change-Id: Ia4c95f5d4069ead2a8cbddfbb924a5f828dfbdea
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

--- common.mk
+++ common.mk
@@ -57,7 +57,8 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    audio.a2dp.default
+    audio.a2dp.default \
+    libaacwrapper
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml
@@ -80,7 +81,8 @@ PRODUCT_PACKAGES += \
 
 # Display
 PRODUCT_PACKAGES += \
-    libvulkan
+    libvulkan \
+    vendor.display.config@1.0
 
 # HotwordEnrollement app permissions
 PRODUCT_COPY_FILES += \
@@ -148,3 +150,10 @@ PRODUCT_STATIC_BOOT_CONTROL_HAL := \
 
 PRODUCT_PACKAGES_DEBUG += \
     update_engine_client
+
+# WiFi Display
+PRODUCT_PACKAGES += \
+    libnl
+
+PRODUCT_BOOT_JARS += \
+    WfdCommon
