1d87f5dcc5dec5660e512abbf724397d8b056feb
Anirudh Gupta <anirudhgupta109@gmail.com>
2020-02-01 19:35:22 +0530

    sdm845-common: DeviceSettings: Add Touchscreen Gesture controls
    Change-Id: I08064ca6c7a7e58783847677843522b818c583de
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

--- DeviceSettings/src/com/aosip/device/DeviceSettings/Startup.java
+++ DeviceSettings/src/com/aosip/device/DeviceSettings/Startup.java
@@ -25,7 +25,11 @@ import android.provider.Settings;
 import android.text.TextUtils;
 import androidx.preference.PreferenceManager;
 
+import com.aosip.device.DeviceSettings.TouchscreenGestureSettings;
+
 public class Startup extends BroadcastReceiver {
+
+    private static final String ONE_TIME_TUNABLE_RESTORE = "hardware_tunable_restored";
 
     @Override
     public void onReceive(final Context context, final Intent bootintent) {
@@ -35,6 +39,7 @@ public class Startup extends BroadcastReceiver {
         VibratorNotifStrengthPreference.restore(context);
 
         boolean enabled = false;
+        TouchscreenGestureSettings.MainSettingsFragment.restoreTouchscreenGestureStates(context);
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
         enabled = sharedPrefs.getBoolean(DeviceSettings.KEY_SRGB_SWITCH, false);
         restore(SRGBModeSwitch.getFile(), enabled);
@@ -62,5 +67,15 @@ public class Startup extends BroadcastReceiver {
             return;
         }
         Utils.writeValue(file, value);
+    }
+
+    private boolean hasRestoredTunable(Context context) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        return preferences.getBoolean(ONE_TIME_TUNABLE_RESTORE, false);
+    }
+
+    private void setRestoredTunable(Context context) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        preferences.edit().putBoolean(ONE_TIME_TUNABLE_RESTORE, true).apply();
     }
 }
