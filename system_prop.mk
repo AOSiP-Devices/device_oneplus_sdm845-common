032cbfe04ae5d93733be8056f93fa7de4d4e912f
maxwen <max.weninger@gmail.com>
2020-02-01 22:46:52 +0100

    sdm845-common: system_prop: Set ro.boot.vendor.overlay.static false
    Change-Id: I4785169c55089aa8581dd6bd39961eeeb2d040b9
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -73,6 +73,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.qti.core_ctl_max_cpu=4 \
     ro.vendor.qti.core_ctl_min_cpu=2
 
+# Quickswitch
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.boot.vendor.overlay.static=false
+
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.radio.bar_fake_gcell=1 \
